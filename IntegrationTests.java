0cda783fc8ac67038b74e48011f0e28400f6364d
Tyler Nielson <nielson@users.campus>
2018-04-23 14:10:08 -0500

    finished integration tests. one failing. 90% coverage

--- IntegrationTests.java
+++ IntegrationTests.java
@@ -60,4 +60,66 @@ public class IntegrationTests {
         vendingMachine.dispenseSelection();
         assertEquals(expectedCount, vendingMachine.stock.get(3).count);
     }
+
+    @Test
+    void test_dispense_selection_and_dispense_change() {
+        VendingMachine.State expectedState = VendingMachine.State.DISPENSE;
+        VendingMachine.State expectedState2 = VendingMachine.State.CHANGE;
+        double expectedBalance = 0;
+        vendingMachine.balance = 3;
+        ByteArrayInputStream in = new ByteArrayInputStream(("3\n").getBytes());
+        System.setIn(in);
+        vendingMachine.select();
+        assertEquals(expectedState, vendingMachine.status);
+
+        vendingMachine.dispenseSelection();
+        assertEquals(expectedState2, vendingMachine.status);
+
+        vendingMachine.dispenseChange();
+        assertEquals(expectedBalance, vendingMachine.balance);
+    }
+
+    @Test
+    void test_start_and_insert_money() {
+        VendingMachine.State expectedState = VendingMachine.State.INSERT;
+        double expectedBalance = 1;
+        ByteArrayInputStream in = new ByteArrayInputStream(("1\n").getBytes());
+        System.setIn(in);
+        vendingMachine.start();
+        assertEquals(expectedState, vendingMachine.status);
+
+        ByteArrayInputStream in2 = new ByteArrayInputStream(("1\n").getBytes());
+        System.setIn(in2);
+        vendingMachine.insertMoney();
+        assertEquals(expectedBalance, vendingMachine.balance);
+    }
+
+    @Test
+    void test_start_and_restock() {
+        VendingMachine.State expectedState = VendingMachine.State.STOCK;
+        double expectedMaxCost = 2;
+        ByteArrayInputStream in = new ByteArrayInputStream(("r\n").getBytes());
+        System.setIn(in);
+        vendingMachine.start();
+        assertEquals(expectedState, vendingMachine.status);
+
+        ByteArrayInputStream in2 = new ByteArrayInputStream(("sampleStock\n").getBytes());
+        System.setIn(in2);
+        vendingMachine.restock();
+        assertEquals(expectedMaxCost, vendingMachine.maxCost);
+    }
+
+    @Test
+    void test_select_and_refund() {
+        VendingMachine.State expectedState = VendingMachine.State.CHANGE;
+        double expectedBalance = 0;
+        vendingMachine.balance = 0.75;
+        ByteArrayInputStream in = new ByteArrayInputStream(("r\n").getBytes());
+        System.setIn(in);
+        vendingMachine.select();
+        assertEquals(expectedState, vendingMachine.status);
+
+        vendingMachine.dispenseChange();
+        assertEquals(expectedBalance, vendingMachine.balance);
+    }
 }
